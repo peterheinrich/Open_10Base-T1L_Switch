d275f5d689ce1c710822bd912391ef889c7d8713
peterheinrich <peter.heinrich@its-heinrich.ch>
2023-04-12 18:41:39 +0200

    Restructuring Repo

--- doc/download_datasheets.sh
+++ doc/download_datasheets.sh
@@ -2,7 +2,7 @@
 
 # Download datasheets mentioned in local libraries
 mkdir -p datasheets
-for i in $(find ../lib/ -type f);do 
+for i in $(find ../kicad/lib/ -type f);do 
 	URL=$(cat $i | grep Datasheet | cut -d "\"" -f 4); 
 	FILE=datasheets/$(echo $i | sed -e "s/.*\/\(.*\)\.kicad_sym/\1/g").pdf; 
 	if [ ! -f $FILE ]; then
